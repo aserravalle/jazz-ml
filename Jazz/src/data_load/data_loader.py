1cb6988a8af10ebe32d86be6093017e19ad2a871
Ariel <aserravalle>
2021-12-16 18:48:45 +1100

    init preprocessor

--- Jazz/src/data_load/data_loader.py
+++ Jazz/src/data_load/data_loader.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import Dict
 import pandas as pd
 
@@ -6,5 +6,3 @@ class DataLoader(ABC):
     @property                 
     def raw_data(self) -> Dict[str, pd.DataFrame]:     
         return self._raw_data
-
-        
